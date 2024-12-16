00278fc3aeca6337333a783d2495deadf9215e18
Katanon Krengsomrong <66160196@go.buu.ac.th>
2024-12-16 22:40:07 +0700

    Component Life Cycle and Props

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ num }}</div>
+  <div>{{ data }}</div>
+  <div><button @click="dec">-</button><button @click="inc">+</button></div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const props = defineProps<{
+  num: number
+}>()
+onMounted(()=>{
+  data.value = props.num
+})
+const data = ref(2)
+const inc = function(){
+  data.value++
+}
+const dec = () => {
+  data.value--
+}
+</script>
