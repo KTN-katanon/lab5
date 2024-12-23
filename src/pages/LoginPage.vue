1d3235ba29f976f29cb2802a7dfa9989eae16f7a
Katanon Krengsomrong <66160196@go.buu.ac.th>
2024-12-23 21:47:00 +0700

    Update LoginPage.vue
    Add style max-width fixed-center

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,29 +1,31 @@
 <template>
-  <q-page padding class="q-pa-md fixed-center" style="max-width: 400px;">
-    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-      <q-input
-        filled
-        v-model="email"
-        label="Your email *"
-        hint="Email"
-        lazy-rules
-        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-      />
+  <q-page padding class="q-pa-md">
+    <div style="max-width: 400px" class="fixed-center">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input
+          filled
+          v-model="email"
+          label="Your email *"
+          hint="Email"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+        />
 
-      <q-input
-        filled
-        type="password"
-        v-model="password"
-        label="Your password *"
-        lazy-rules
-        :rules="[(val) => (val !== null && val !== '') || 'Please type your password']"
-      />
+        <q-input
+          filled
+          type="password"
+          v-model="password"
+          label="Your password *"
+          lazy-rules
+          :rules="[(val) => (val !== null && val !== '') || 'Please type your password']"
+        />
 
-      <div>
-        <q-btn label="Submit" type="submit" color="primary" />
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+    </div>
   </q-page>
 </template>
 
