90d6ab6eda49081c31954299ed0ea027072c0dbf
Katanon Krengsomrong <66160196@go.buu.ac.th>
2024-12-23 21:29:01 +0700

    Create LoginPage.vue

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page padding>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="email"
+        label="Your email *"
+        hint="Email"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Your password *"
+        lazy-rules
+        :rules="[(val) => (val !== null && val !== '') || 'Please type your password']"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+function
+
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+          })
+
+function  onReset () {
+        name.value = null
+        age.value = null
+        accept.value = false
+      }
+</script>
